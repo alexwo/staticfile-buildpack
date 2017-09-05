cce3dedf1f4c98491b53d6fa439663c8053bf5a8
Firas Najjar <firas.najjar@sap.com>
2017-09-05 16:31:48 +0300

    temp

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -7,8 +7,13 @@ wget https://buildpacks.cloudfoundry.org/dependencies/node/node-8.3.0-linux-x64-
 tar xvf node-8.3.0-linux-x64-8e2f91f8.tgz
 export PATH="$(pwd)/node-v8.3.0-linux-x64/bin:$PATH"
 
+echo 'Path: $(PATH)'
+echo 'node version'
+node -v
+
+npm init -f
 npm install nodemon
 npm install cfwizz
 npm install redbird
 echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
-node ./node_modules/cfwizz/boot.js
+node ./node_modules/cfwizz/boot_static_file.js
